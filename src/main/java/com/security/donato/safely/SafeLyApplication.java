0be03e20aebd156da3ec0fe8a56ce03cdd402c7d
Matheus Di Donato <matheusdidonato@yahoo.com.br>
2022-03-12 19:01:36 -0300

    initial commit

---
+++ src/main/java/com/security/donato/safely/SafeLyApplication.java
@@ -0,0 +1,13 @@
+package com.security.donato.safely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeLyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeLyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/donato/safely/SafeLyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.donato.safely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeLyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
